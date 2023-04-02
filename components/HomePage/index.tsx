5c3a64aac9bb6129bf708cbf5caf9092290b9efd
NGOC DUONG <ngocduong6697@gmail.com>
2023-04-02 14:52:33 +0700

    feat: add api and itegrate api get transaction

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -1,6 +1,56 @@
-import Head from 'next/head'
+import React, { useState } from 'react'
+import axios from 'axios'
+import SearchTransactionForm from '@app/components/Transaction/SearchTransactionForm'
+import TransactionList from '@app/components/Transaction/TransactionList'
 
 const HomePage = () => {
-  return <h1 className="text-2xl">Solana Blockchain Explorer</h1>
+  const [loading, setLoading] = useState(false)
+  const [transactionList, setTransactionList] = useState<[]>([])
+  const [balance, setBalance] = useState<number>(0)
+  const [address, setAddress] = useState<string>('')
+  const [errorMessage, setErrorMessage] = useState<any>('')
+
+  const handleFormSubmit = async (event: any) => {
+    try {
+      event.preventDefault()
+      setLoading(true)
+      setErrorMessage('')
+
+      const response = await axios.get(`/api/transactions/?address=${address}`)
+      if (response.status === 200) {
+        setTransactionList(response.data.transactionList)
+        const accountBalanceText = response.data.accountBalance
+        const accountBalance = parseInt(accountBalanceText) / 1_000_000_000
+
+        accountBalance && setBalance(accountBalance)
+      }
+    } catch (error: any) {
+      console.log('client', error)
+      setErrorMessage(
+        error?.response?.data?.message || 'Unable to fetch transactions. Please try again later.'
+      )
+    } finally {
+    }
+
+    setLoading(false)
+  }
+  return (
+    <main className="w-full h-full max-w-2xl p-6 flex flex-col items-center justify-between gap-6 mx-auto relative">
+      <h1 className="text-2xl">Solana Blockchain Explorer</h1>
+      <SearchTransactionForm
+        handleFormSubmit={handleFormSubmit}
+        address={address}
+        setAddress={setAddress}
+        loading={loading}
+        errorMessage={errorMessage}
+      />
+
+      <TransactionList transactionList={transactionList} balance={balance} />
+
+      {loading && (
+        <div className="absolute inset-0 bg-white/70 flex items-center justify-center">Loading</div>
+      )}
+    </main>
+  )
 }
 export default HomePage
---
+++ pages/api/transactions.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as solanaWeb3 from '@solana/web3.js'
+
+const DEV_NET = solanaWeb3.clusterApiUrl('devnet')
+const solanaConnection = new solanaWeb3.Connection(DEV_NET)
+
+const getAddressInfo = async (address: any, numTx = 3) => {
+  const pubKey = new solanaWeb3.PublicKey(address)
+  const transactionList = await solanaConnection.getSignaturesForAddress(pubKey, { limit: numTx })
+  const accountBalance = await solanaConnection.getBalance(pubKey)
+
+  return { transactionList, accountBalance }
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const queryAddress = req.query?.address
+  if (!queryAddress) {
+    return res.status(401).json({
+      message: 'Invalid address',
+    })
+  }
+  try {
+    const { accountBalance, transactionList } = await getAddressInfo(queryAddress)
+    return res.status(200).json({ transactionList, accountBalance })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: 'Something went wrong. Please try again later',
+    })
+  }
+}
+
+export default handler
